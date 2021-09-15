3c41c5d56f4607425cfd9fd41709167e3d6d38e1
John Rowley <outsidethebox@eircom.net>
2021-09-15 17:12:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SuperHero03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Powers = new string[] { "Run at mac5", "go back in the past" };
+
+            SuperHero superHero = new SuperHero("Barry Allen", "The Flash", Powers);
+
+            string description = superHero.ToString();
+
+            Console.WriteLine(description);
+
+
+            superHero.DisplayPowers();
+
+            //===================================
+            SuperHero superHero2 = new SuperHero();
+            superHero2.Alias = "Clarke Kent";
+            superHero2.HeroName = "SuperMan";
+            superHero2.Powers = new string[] { "X-Ray Vision", "Flight" };
+
+            Console.WriteLine(superHero2.ToString());
+
+
+            superHero2.DisplayPowers();
+
+
+
+        }
+    }
+}
---
+++ SuperHero.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero03
+{
+    //string myName = "Barry Allen";
+    //string codeName = "The Flash";
+    //string[] Ability = new string[] { "Run at mac5", "go back in the past" }
+    public class SuperHero
+    {
+
+      
+        public string Alias { get; set; }
+        public string HeroName { get; set; }
+
+        public string[] Powers { get; set; }
+
+
+        public SuperHero()
+        {
+
+        }
+
+        public SuperHero(string _alias, string _heroName, string[] _powers)
+        {
+            Alias = _alias;
+            HeroName = _heroName;
+            Powers = _powers;
+        }
+
+        public override string ToString()
+        {
+
+           return  $"my name is {Alias} also known as {HeroName}";
+
+        }
+
+        public void DisplayPowers()
+        {
+
+            foreach (string powers in Powers)
+            {
+                Console.WriteLine($"I am able to {powers}");
+            }
+
+        }
+    }
+}
